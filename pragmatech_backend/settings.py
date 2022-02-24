9456a09e3baa8c046b49a0af1d6a54807bab12a0
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-02-24 14:28:08 -0500

    debug

--- pragmatech_backend/settings.py
+++ pragmatech_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-kf#o=hkk7++md-48o6yek5wz+pbs_7o0==%hcqhp)lfr!9*d&b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
