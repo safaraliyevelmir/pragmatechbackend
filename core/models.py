8102445bd169d59283255079b879394e4566361a
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-03-03 03:44:51 -0500

    slider image

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class AboutTimeLine(models.Model):
 
     title = models.CharField(max_length=255)
     about = models.TextField()
-    image = models.ImageField(upload_to='about/')
+    image = models.ImageField(upload_to='about/',null=True,blank=True)
     index = models.IntegerField(primary_key=True,unique=True,help_text="Entered number you want this object  be in what order")
 
     def __str__(self):
