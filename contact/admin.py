98b6c38d22e68ba0fe9c23276863f4054b1a51a4
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-03-03 03:53:00 -0500

    admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 
-from .models import ApllyCourse, Contact
+# from .models import ApllyCourse, Contact
 
-admin.site.register(Contact)
-admin.site.register(ApllyCourse)
+# admin.site.register(Contact)
+# admin.site.register(ApllyCourse)
