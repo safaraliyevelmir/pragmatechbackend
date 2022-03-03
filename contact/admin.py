449b39ad2964f8b1f33f575c4178dfae6362c0a5
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-03-03 10:25:15 -0500

    statics

--- contact/admin.py
+++ contact/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 
 from .models import ApllyCourse, Contact
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+admin.site.register(Contact,ContactAdmin)
 admin.site.register(ApllyCourse)
