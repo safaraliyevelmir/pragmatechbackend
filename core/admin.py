8d53394fbd7ba9411762d32e51252dc5c88443db
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-03-08 14:38:04 -0500

    fake

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 
-from .models import AboutTimeLine, Slider
+from .models import AboutTimeLine, Slider,AboutPageContent, ProgramPageContent
 
 
 class AboutTimeLineAdmin(admin.ModelAdmin):
 
     list_display = ('title','index',)
 
-# admin.site.register(AboutPageContent)
-# admin.site.register(ProgramPageContent)
+admin.site.register(AboutPageContent)
+admin.site.register(ProgramPageContent())
 admin.site.register(AboutTimeLine,AboutTimeLineAdmin)
 admin.site.register(Slider)
-# admin.site.register(AboutPageContent)
