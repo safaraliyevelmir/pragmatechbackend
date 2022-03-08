bf5607360834068991964c97298fc9d45e82a67b
safaraliyevelmir <safaraliyevelmir@gmail.com>
2022-03-08 14:37:06 -0500

    fake

--- core/models.py
+++ core/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
-# class AboutPageContent(models.Model):
-#     content = models.TextField()
-#     image = models.ImageField(upload_to="about/")
+class AboutPageContent(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to="about/")
     
-#     def __str__(self):
-#         return f"About page {self.pk}"
+    def __str__(self):
+        return f"About page {self.pk}"
     
-# class ProgramPageContent(models.Model):
-#     content = models.TextField()
-#     image = models.ImageField(upload_to="about/")
+class ProgramPageContent(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to="about/")
     
-#     def __str__(self):
-#         return f"Program page {self.pk}"
+    def __str__(self):
+        return f"Program page {self.pk}"
       
 class AboutTimeLine(models.Model):
 
